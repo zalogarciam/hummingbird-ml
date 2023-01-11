77cf10740e49b408ab9d607676d817e23245ce13
zalogarciam <gegarciam95@gmail.com>
2023-01-11 17:30:50 -0500

    Adding properties for FlowerArea class

--- Hummingbirds/Assets/Hummingbird/Scripts/FlowerArea.cs
+++ Hummingbirds/Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///     Manages a collection of flower plants and attached flowers
+/// </summary>
 public class FlowerArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
