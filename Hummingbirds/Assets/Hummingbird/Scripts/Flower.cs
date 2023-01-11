754b2d9882389753fd3970d0233d9f90d47997b9
zalogarciam <gegarciam95@gmail.com>
2023-01-11 16:01:14 -0500

    Implement reset flower method

--- Hummingbirds/Assets/Hummingbird/Scripts/Flower.cs
+++ Hummingbirds/Assets/Hummingbird/Scripts/Flower.cs
@@ -89,4 +89,20 @@ public class Flower : MonoBehaviour
         // Return the amount of nectar that was taken
         return nectarTaken;
     }
+
+    /// <summary>
+    ///  Resets the flower
+    /// </summary>
+    public void ResetFlower() 
+    {
+        // Refill the nectar
+        NectarAmount = 1f;
+
+        // Enable the flower and nectar colliders
+        flowerCollider.gameObject.SetActive(true);
+        nectarCollider.gameObject.SetActive(true);
+
+        // Change the flower color to inidicate that it is full
+        flowerMaterial.SetColor("_BaseColor", fullFlowerColor);
+    }
 }
