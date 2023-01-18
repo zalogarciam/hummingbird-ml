546883e610be1a972c659745b8339a7d80a2db81
zalogarciam <gegarciam95@gmail.com>
2023-01-18 17:25:26 -0500

    Implementing methods in FlowerArea: Awake, Start, GetFlowerFromNectar and ResetFlower

--- Hummingbirds/Assets/Hummingbird/Scripts/FlowerArea.cs
+++ Hummingbirds/Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -5,4 +6,19 @@ using UnityEngine;
 /// </summary>
 public class FlowerArea : MonoBehaviour
 {
+    // The diameter of the are where the agent and flowers can be
+    // user for observing relative distance from agent to flower
+
+    public const float AreaDiameter = 20f;
+
+    // The list of all flower plants in thus flower area (flower plants have multiple flowers)
+    private List<GameObject> flowerPlants;
+
+    // A lookup dictionary for looking up a flower from a nectar collider
+    private Dictionary<Collider, Flower> nectarFlowerDictionary;
+
+    /// <summary>
+    /// List of all flowers in the flower area
+    /// </summary>
+    public List<Flower> Flowers { get; private set; }
 }
